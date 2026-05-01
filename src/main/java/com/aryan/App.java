aac1609fa64855a39b25990bb6bcd0030394fdbe
ARYAN JAISWAL <143169615+ARYAN-3345@users.noreply.github.com>
2026-05-01 21:34:40 +0530

    Update greeting message in App.java

--- src/main/java/com/aryan/App.java
+++ src/main/java/com/aryan/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello From Maven Jar! Aryan Jaiswal" );
+        System.out.println( "Hello From Maven Jar! Aryan Jaiswal namaste" );
     }
 }
